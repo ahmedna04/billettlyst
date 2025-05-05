fcc69d0304a22ca30362ba117771a194efddd6d2
ahmedna04 <ahmedna@hiof.no>
2025-05-05 14:25:30 +0200

    Ble ferdig med E kravene, har lagt til navigasjon og routing

--- billettlyst/src/App.jsx
+++ billettlyst/src/App.jsx
@@ -3,12 +3,17 @@ import Home from './sider/home';
 import EventPage from './sider/eventpage';
 import CategoryPage from './sider/categorypage';
 import Dashboard from './sider/dashboard';
+import Nav from './komponenter/nav';
+
+
 
 function App() {
   return (
     <>
+    <Nav/>
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/event/:id" element={<EventPage />} />
         <Route path="/category/:slug" element={<CategoryPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
---
+++ billettlyst/src/komponenter/nav.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+function Nav() {
+  return (
+    <nav>
+      <Link to="/">Billettlyst</Link>
+      <Link to="/category/musikk">Musikk</Link>
+      <Link to="/category/sport">Sport</Link>
+      <Link to="/category/teater">Teater</Link>
+      <Link to="/dashboard">Logg inn</Link>
+    </nav>
+  );
+}
+
+export default Nav;
