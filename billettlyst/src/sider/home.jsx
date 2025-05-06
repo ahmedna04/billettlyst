cde31c7a48de6b2d4cdbcdd2039299581186a22d
ahmedna04 <ahmedna@hiof.no>
2025-05-06 16:35:32 +0200

    Lagt inn "useEffect" for å hente festivaler, laget enkel midlertidig css for nå

--- billettlyst/src/sider/home.jsx
+++ billettlyst/src/sider/home.jsx
@@ -1,4 +1,45 @@
+import { useEffect, useState } from 'react';
+import { fetchEventByKeyword } from '../api/ticketmaster';
+import '../styles/home.css';
+
 function Home() {
-    return <h1>Home – Forsiden</h1>;
-  }
-  export default Home;
+  
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    async function hentFestivaler() {
+      const festivalNavn = ['Findings', 'Neon', 'Skeikampenfestivalen', 'Tons of Rock'];
+      const alleFestivaler = [];
+  
+      for (const navn of festivalNavn) {
+      const resultater = await fetchEventByKeyword(navn);
+      console.log("Søker etter:", navn, "Fant:", resultater.length, "resultater"); // 👈 Denne linja
+      if (resultater.length > 0) {
+      alleFestivaler.push(resultater[0]);
+      }
+      }
+  
+      setEvents(alleFestivaler);
+    }
+  
+    hentFestivaler();
+  }, []);
+  return (
+
+    <main>
+      <h1>Festivaler</h1>
+      <section className="event-list">
+        {events.map((event) => (
+          <article key={event.id} className="event-card">
+            <h2>{event.name}</h2>
+            <p>{event.dates?.start?.localDate}</p>
+            <p>{event._embedded?.venues?.[0]?.city?.name}, {event._embedded?.venues?.[0]?.country?.name}</p>
+            <a href={event.url} target="_blank" rel="noopener noreferrer">
+            </a>
+          </article>
+        ))}
+      </section>
+    </main>
+  );
+}
+export default Home;
+
