a65c0d6a1fc05dd01a1d36e18a282f3b35edc962
ahmedna04 <ahmedna@hiof.no>
2025-05-06 21:06:04 +0200

    Har problem med å printe ut eventer, API printer ut bare 2 istedenfor 4, tror problemet ligger hos ticketmaster.

--- billettlyst/src/sider/eventpage.jsx
+++ billettlyst/src/sider/eventpage.jsx
@@ -1,5 +1,37 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+const API_KEY = 'dW9FyNOAXIgM1BsAEXhrWIBJwVg34bin'; 
+
 function EventPage() {
-    return <h1>EventPage – Detaljer om arrangement</h1>;
-  }
-  export default EventPage;
-  
+
+  const {id} = useParams();
+
+  const [event, setEvent] = useState(null);
+
+  useEffect(() => {
+    async function hentEvent() {
+      try {
+        const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=${API_KEY}`);
+        const data = await response.json();
+        setEvent(data);
+      } catch (error) {
+        console.error("Feil ved henting av event:", error);
+      }
+    }
+
+    hentEvent();
+  }, [id]);
+
+  if (!event) return <p>Laster</p>;
+
+  return (
+    <main>
+      <h1>{event.name}</h1>
+      <p>{event.dates?.start?.localDate}</p>
+      <p>{event._embedded?.venues?.[0]?.city?.name}, {event._embedded?.venues?.[0]?.country?.name}</p>
+      <a href={event.url} target="_blank" rel="noopener noreferrer">Kjøp billetter</a>
+    </main>
+  );
+}
+
+export default EventPage;
