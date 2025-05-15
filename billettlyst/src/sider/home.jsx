eed62e7609016b839208f16aac0a5b53aea5041d
ahmedna04 <ahmedna@hiof.no>
2025-05-15 19:46:28 +0200

    Har lagt til dynamisk byvalg med knapper Oslo, Berlin, Paris og london,

--- billettlyst/src/sider/home.jsx
+++ billettlyst/src/sider/home.jsx
@@ -5,23 +5,12 @@ import '../styles/home.css';
 
 function Home() {
   const [events, setEvents] = useState([]);
+
   const [byEvents, setByEvents] = useState([]);
 
-  
-  async function hentByEvents() {
-    try {
-      const response = await fetch(
-        `https://app.ticketmaster.com/discovery/v2/events.json?city=Oslo&size=10&apikey=dW9FyNOAXIgM1BsAEXhrWIBJwVg34bin`
-      );
-      const data = await response.json();
-      setByEvents(data._embedded?.events || []);
-      console.log("Oslo-events:", data._embedded?.events);
-    } catch (error) {
-      console.log("Kan ikke hente", error);
-    }
-  }
+  const [valgtBy, setValgtBy] = useState(null); 
 
-  
+  // To festivaler vil ikke komme opp på nettsiden, på grunn av error fra ticketmaster jeg har skrevet mer om det på rapporten
   async function hentFestivaler() {
     const festivalNavn = ['Findings', 'Neon', 'Skeikampenfestivalen', 'Tons of Rock'];
     const alleFestivaler = [];
@@ -37,35 +26,63 @@ function Home() {
     setEvents(alleFestivaler);
   }
 
+  
+  async function hentByEvents(byNavn) {
+    try {
+      const response = await fetch(
+        `https://app.ticketmaster.com/discovery/v2/events.json?city=${byNavn}&size=10&apikey=dW9FyNOAXIgM1BsAEXhrWIBJwVg34bin`
+      );
+      const data = await response.json();
+
+      setByEvents(data._embedded?.events || []);
+      setValgtBy(byNavn); 
+      console.log("Hentet events for", byNavn, ":", data._embedded?.events);
+    } catch (error) {
+      console.log("Kan ikke hente for by", byNavn, error);
+    }
+  }
+
   useEffect(() => {
-    hentFestivaler();
-    hentByEvents();
+
+    hentFestivaler(); 
   }, []);
 
+ 
   return (
     <main>
       <h1>Festivaler</h1>
+
       <section className="event-list">
         {events.map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
       </section>
 
-      <h2>Oslo</h2>
-
-      <section className="oslo-event-list">
-
-        {byEvents.map((event) => (
-          <article key={event.id}>
-            <h3>{event.name}</h3>
-            <p>{event.dates?.start?.localDate}</p>
-            <p>
-              {event._embedded?.venues?.[0]?.city?.name},{" "}
-              {event._embedded?.venues?.[0]?.country?.name}
-            </p>
-          </article>
-        ))}
+      <section className="byknapper">
+        <button onClick={() => hentByEvents('Oslo')}>Oslo</button>
+        <button onClick={() => hentByEvents('Paris')}>Paris</button>
+        <button onClick={() => hentByEvents('London')}>London</button>
+        <button onClick={() => hentByEvents('Berlin')}>Berlin</button>
       </section>
+
+      {valgtBy && (
+        <>
+          <h2>I {valgtBy} kan du se:</h2>
+
+          <section className="oslo-event-list">
+            {byEvents.map((event) => (
+              <article key={event.id}>
+                <h3>{event.name}</h3>
+                <p>{event.dates?.start?.localDate}</p>
+                <p>
+                  {event._embedded?.venues?.[0]?.city?.name},{" "}
+                  {event._embedded?.venues?.[0]?.country?.name}
+                </p>
+              </article>
+            ))}
+          </section>
+        </>
+      )}
     </main>
   );
 }
